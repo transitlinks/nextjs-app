75f2c9447f316b387957b604156b422221f0e520
Vladimir Halme <vhalme@gmail.com>
2021-04-05 02:04:23 +0300

    Added debug output

--- src/components/Account/Profile.tsx
+++ src/components/Account/Profile.tsx
@@ -27,6 +27,8 @@ const Profile = ({ user }: ProfileProps) => {
 
   let { uuid } = profileUser || {};
 
+  console.log('USER IN PROFILE', user, profileUser);
+
   const [emailInput, setEmailInput] = useState<TextInputState>({ value: user?.email || '', pass: true });
   const [profileSettingsInput, setProfileSettingsInput] = useState<ProfileSettingsInputState>({
     value: {
@@ -82,6 +84,8 @@ const Profile = ({ user }: ProfileProps) => {
 
   const valuesValid = emailInput.pass && profileSettingsInput.pass;
 
+  console.log('input values', emailInput, profileSettingsInput);
+  
 	return (
     <div>
       <div>
